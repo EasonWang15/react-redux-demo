e65c4bbffa1ed156a70a81f54cc01fcb93d2f067
Eason <13709310753@163.com>
2019-08-05 15:55:45 +0800

    build

---
+++ build/precache-manifest.267d735eadac53963178e4f6d37f25e6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e860203cec7c0d8a20b5f46323367587",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a1fecd50875a9df38ff",
+    "url": "/static/js/2.179db301.chunk.js"
+  },
+  {
+    "revision": "59f4667a0aa2e1f3e209",
+    "url": "/static/js/main.14b110a7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
